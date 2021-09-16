d0a1a88b4ff07719a5446ca6048538d9523997d5
Angelina <donchenko.a11@gmail.com>
2021-09-16 20:36:57 +0300

    [feature/practice]: Lecture progress

---
+++ src/main/java/practice/PrimitiveTypes.java
@@ -0,0 +1,69 @@
+package practice;
+
+public class PrimitiveType {
+
+    public static void main(String[] args) {
+
+        // Dopuble primitive type practice
+
+
+
+        double doubleMinValue = Double.MIN_VALUE;
+        System.out.println(doubleMinValue);
+
+        double doubleMaxValue = Double.MAX_VALUE;
+        System.out.println(doubleMinValue);
+
+        double doubleValue =
+
+
+
+
+
+
+
+                // Long primitive type practice
+
+                // Int primitive type practice
+        int minIntValue = Integer.MIN_VALUE;
+        System.out.println(minIntValue);
+
+        int maxValue = Integer.MAX_VALUE;
+        System.out.println(maxValue);
+
+        int firstVariable = 200;
+        int secondVariable = 400;
+
+        System.out.println(Integer.max(firstVariable, secondVariable));
+
+
+
+        // Method call
+
+        printHelloWorld();
+
+        //String
+
+        String myName ="Angelina";
+        String lastName = "Donchenko";
+        String namrAndLastname = myName + lastName;
+        System.out.println(myName + " " + lastName);
+        System.out.printf("%s %s\n", myName, lastName);
+        String nameAndUserName = String.fornat("%s %s\n", myName, lastName);
+
+
+
+        // Boolean
+
+        boolean isStudent = false;
+
+
+
+
+    }
+
+    public static void printHelloWorld() {
+        System.out.println("HelloWorld");
+    }
+}
+
